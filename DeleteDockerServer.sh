025c629525826016e4ab76f186bf6ed3386671e7
Alberto Cuesta Cañada <mail@albertocuesta.es>
2018-06-14 10:29:13 +0100

    Deleting renamed files

--- DeleteDockerServer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PATH=$PATH:/home/alberto/AWS/Git/AWS-Tooling/
-
-aws cloudformation delete-stack \
-    --stack-name DockerServerDokuwiki
-waitForDeletion.py -s DockerServerDokuwiki
-beep
-
